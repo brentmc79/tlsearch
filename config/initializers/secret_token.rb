f338b0cf96161732b5356998f185b2df52d06a7b
Brent Collier <bcollier@twitter>
2012-11-27 22:21:58 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tlsearch::Application.config.secret_token = 'b01988e620d8159e390e2f9fd90ec16fa3c539e90761307f9e7c13db74a0cc4fc26507e7e73854da0bf9834af8d7b8d2d3ace07382b7ffbd81e79acb80fe3518'
